7e7b5410db82acecea34a21a90a0a60048dbda08
Will Nation <wnation@utexas.edu>
2016-06-22 13:39:13 -0500

    basic layout for MainActivity. added click method for button

--- app/src/main/java/com/example/wnati/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/wnati/simpletodo/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.wnati.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems; //list view handle
+
     @Override
+    /*
+     * set XML layout for activity
+     * attach adapter to ListView
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvItems = (ListView)findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+
+        items.add("Complete Android tutorial");
+        items.add("Get accepted to CodePath bootcamp");
+    }
+
+    /*
+     * add text input item when Add Button is clicked
+     */
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText)findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
     }
 }
