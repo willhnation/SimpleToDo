c9f2917466d7681d02607ec375871e5b164b8b79
Will Nation <wnation@utexas.edu>
2016-06-22 21:05:25 -0500

    added read/write to todo.txt. removed test items

--- app/src/main/java/com/example/wnati/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/wnati/simpletodo/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.wnati.simpletodo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,12 +30,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvItems = (ListView)findViewById(R.id.lvItems);
-        items = new ArrayList<>();
+        readItems();
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
 
-        items.add("Complete Android tutorial");
-        items.add("Get accepted to CodePath bootcamp");
+        setupListViewListener();
+    }
+
+    // setup click listener
+    private void setupListViewListener() {
+        /*
+         * add a LongClickListener for each Item in the ListView
+         * Item will be removed and the adapter will be refreshed
+         */
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
     }
 
     /*
@@ -41,5 +62,28 @@ public class MainActivity extends AppCompatActivity {
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText("");
+        writeItems();
+    }
+
+    // read items from todo.txt, otherwise return an empty list
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    // write the items to todo.txt
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
